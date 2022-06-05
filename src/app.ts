eb6edba6a94079ca83135a85c736a599ed449502
Emilbek <emil@Emilbeks-MacBook-Air.local>
2022-06-05 11:16:51 +0600

    init

--- src/app.ts
+++ src/app.ts
@@ -76,7 +76,6 @@ const buildApp = (opt: FastifyServerOptions) => {
     connectionString: `postgres://${user}:${password}@${host}/${database}`,
   });
   app.register(fastifyCors, {
-    // origin: "http://ts.kse.kg",
     origin: "http://localhost:8080",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     preflightContinue: false,
